53dccd55b515912710b40e09e810a0a524dc33f3
SooDevv <soojung.dev@gmail.com>
2021-01-18 18:01:34 +0900

    add step19.py

---
+++ steps/step19.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data, name=None):
+        if not isinstance(data, np.ndarray):
+            raise TypeError
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+        self.name = name
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                x.grad = gx if x.grad is None else x.grad + gx
+                if x.creator is not None:
+                    add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+
+    def clear_grad(self):
+        self.grad = None
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+
+
+if __name__ == '__main__':
+    x = Variable(np.array([1., 2., 3.]))
+    print(x)
+    print(x.shape)
+    print(x.dtype)
+    print(x.ndim)
+    print(len(x))
