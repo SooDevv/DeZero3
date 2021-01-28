471fc67a708b46fbd7e6a017ef35d5e31fdf95a4
SooDevv <soojung.dev@gmail.com>
2021-01-28 22:37:20 +0900

    Add step26.py

--- dezero/utils.py
+++ dezero/utils.py
@@ -1,9 +1,9 @@
-def get_dot_graph():
-    pass
+import os
+import subprocess
 
 
 def _dot_var(v, verbose=False):
-    dot_var = '{} [label="{}", color=orange, style=filled\n'
+    dot_var = '{} [label="{}", color=orange, style=filled]\n'
 
     name = '' if v.name is None else v.name
     if verbose and v.data is not None:
@@ -23,3 +23,44 @@ def _dot_func(f):
     for y in f.outputs:
         txt += dot_edge.format(id(f), id(y()))
     return txt
+
+
+def get_dot_graph(output, verbose=True):
+    txt = ''
+    funcs = []
+    seen_set = set()
+
+    def add_func(f):
+        if f not in seen_set:
+            funcs.append(f)
+            seen_set.add(f)
+
+    add_func(output.creator)
+    txt += _dot_var(output, verbose)
+
+    while funcs:
+        f = funcs.pop()
+        txt += _dot_func(f)
+        for x in f.inputs:
+            txt += _dot_var(x, verbose)
+
+            if x.creator is not None:
+                add_func(x.creator)
+    return 'digraph g {\n' + txt + '}'
+
+
+def plot_dot_graph(output, verbose=True, to_file='graph.png'):
+        dot_graph = get_dot_graph(output, verbose)
+
+        tmp_dir = os.path.join(os.path.expanduser('~'), 'Documents/Git/DeZero3/steps')
+        if not os.path.exists(tmp_dir):
+            os.mkdir(tmp_dir)
+        dot_graph_path = to_file.split('.')[0]
+        graph_path = os.path.join(tmp_dir, f'{dot_graph_path}.dot')  # dezero/steps/step00_graph.dot
+
+        with open(graph_path, 'w') as f:
+            f.write(dot_graph)
+
+        extension = os.path.splitext(to_file)[1][1:]
+        cmd = f'dot {graph_path} -T {extension} -o {to_file}'
+        subprocess.run(cmd, shell=True)
--- steps/step26.py
+++ steps/step26.py
@@ -1,16 +1,33 @@
 import numpy as np
 
 from dezero import Variable
-from dezero.utils import _dot_var, _dot_func
+from dezero.utils import _dot_var, _dot_func, plot_dot_graph
 
 
-x = Variable(np.random.rand(2, 3))
-x.name = 'x'
+a = Variable(np.random.rand(2, 3))
+a.name = 'a'
 
-print(_dot_var(x))
-print(_dot_var(x, verbose=True))
+print(_dot_var(a))
+print(_dot_var(a, verbose=True))
 
-x0 = Variable(np.array(1.0))
-x1 = Variable(np.array(1.0))
-y = x0 + x1
-print(_dot_func(y.creator))
+b = Variable(np.array(1.0))
+c = Variable(np.array(1.0))
+d = b + c
+print(_dot_func(d.creator))
+
+
+def goldstein(x, y):
+    z = (1 + (x + y + 1) ** 2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * \
+        (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
+    return z
+
+
+x = Variable(np.array(1.0), name='x')
+y = Variable(np.array(1.0), name='y')
+z = goldstein(x, y)
+z.backward()
+
+
+z.name = 'z'
+plot_dot_graph(z, verbose=False, to_file='step26_goldstein.png')
+
