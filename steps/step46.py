f38d55a8f5552a24a614b9c157fc3810dc30159d
SooDevv <soojung.dev@gmail.com>
2021-02-17 22:39:51 +0900

    update step46.py

--- steps/step46.py
+++ steps/step46.py
@@ -5,17 +5,21 @@ import dezero.functions as F
 from dezero.models import MLP
 
 
+# prepare dataset
 np.random.seed(2021)
 x = np.random.rand(100, 1)
 y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
 
+# parameter
 lr = 0.2
 max_iter = 10000
 hidden_size = 10
 
-model = MLP((hidden_size, 1))
+# set model & optimizier
+model = MLP((hidden_size, 1), activation=F.sigmoid)
 optimizer = optimizers.SGD(lr).setup(model)
 
+# training
 for i in range(max_iter):
     y_pred = model(x)
     loss = F.mse(y, y_pred)
