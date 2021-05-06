873ee54891402db4d4cd54a41549d4a7a3f1ce02
SooDevv <soojung.dev@gmail.com>
2021-05-06 23:46:41 +0900

    Add step58.py

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -129,3 +129,20 @@ class MNIST(Dataset):
     @staticmethod
     def labels():
         return {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
+
+
+# =============================================================================
+# Big datasets
+# =============================================================================
+class ImageNet(Dataset):
+
+    def __init__(self):
+        NotImplemented
+
+    @staticmethod
+    def labels():
+        url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
+        path = get_file(url)
+        with open(path, 'r') as f:
+            labels = eval(f.read())
+        return labels
---
+++ steps/step58.py
@@ -0,0 +1,19 @@
+import numpy as np
+from PIL import Image
+import dezero
+from dezero.models import VGG16
+
+url = 'https://github.com/oreilly-japan/deep-learning-from-scratch-3/raw/images/zebra.jpg'
+img_path = dezero.utils.get_file(url)
+img = Image.open(img_path)
+x = VGG16.preprocess(img)
+x = x[np.newaxis]
+
+model = VGG16(pretrained=True)
+with dezero.test_mode():
+    y = model(x)
+predict_id = np.argmax(y.data)
+
+model.plot(x, to_file='step58_vgg.pdf')
+labels = dezero.datasets.ImageNet.labels()
+print(labels[predict_id])
